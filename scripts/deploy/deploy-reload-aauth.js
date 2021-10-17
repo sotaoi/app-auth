50461551b7e3c8189c08a9b3dd6a616c6fc34ef9
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 10:09:36 +0300

    refactor: update

---
+++ scripts/deploy/deploy-reload-aauth.js
@@ -0,0 +1,12 @@
+#!/bin/env node
+
+const { execSync } = require('child_process');
+
+const main = async () => {
+  execSync('npm run start:auth:prod', { stdio: 'inherit' });
+  execSync('git checkout -- ./', { stdio: 'inherit' });
+  execSync('git pull', { stdio: 'inherit' });
+  execSync('npm run restart:auth:prod', { stdio: 'inherit' });
+};
+
+main();
