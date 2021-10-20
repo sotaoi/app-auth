e69152474338c5e3cc0570758618f6aaf1240c74
qwertypnk <qwertypnk@protonmail.com>
2021-10-20 15:02:01 +0300

    refactor: update

--- auth.entry.js
+++ auth.entry.js
@@ -1,6 +1,6 @@
 const { init } = require('@app/omni/init');
 init();
-const { main } = require('@app/auth/main');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const { main } = require('@app/auth/main');
 
 main();
